a4de512728d28a48c49e42ddcf562cfe8d3d0e77
Alex <alexander221b@hotmail.com>
2022-05-03 17:47:26 -0500

    ping route

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from products import products
 
 app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return 'Pong'
 
 # if the name of this file is the main one. Run the server in debug mode
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
+
+
