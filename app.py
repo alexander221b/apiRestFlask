d1ee28efcf26c704dae9a10ab8cf07928fce4243
Alex <alexander221b@hotmail.com>
2022-05-04 07:28:48 -0500

    products route

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from products import products
 
 app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
-    return 'Pong'
+    return jsonify({'message': 'pong!'})
+
+@app.route('/products')
+def getProducts():
+    #return jsonify(products)
+    return jsonify({"products": products, "message": "Product's list"})
 
 # if the name of this file is the main one. Run the server in debug mode
 if __name__ == '__main__':
